6afcea1c652ca02146dfbb386190a886a3343002
AlexKazmirchuk <zedomo@mail.ru>
2016-12-05 18:37:38 +0200

    added MainForm

--- src/com/alexkaz/screenshoter/Main.java
+++ src/com/alexkaz/screenshoter/Main.java
@@ -3,6 +3,6 @@ package com.alexkaz.screenshoter;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        new MainForm();
     }
 }
---
+++ src/com/alexkaz/screenshoter/MainForm.java
@@ -0,0 +1,13 @@
+package com.alexkaz.screenshoter;
+
+import javax.swing.*;
+
+public class MainForm extends JFrame {
+
+    MainForm(){
+        super("Screenshoter v1.0");
+        setSize(600,400);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
