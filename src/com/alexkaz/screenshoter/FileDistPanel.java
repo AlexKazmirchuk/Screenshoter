c7fe95ae0dab4278cff07f85ec386d485e05e798
AlexKazmirchuk <zedomo@mail.ru>
2016-12-10 17:36:08 +0200

    now can choose directory for saving screenshots with JFileChooser

--- src/com/alexkaz/screenshoter/FileDistPanel.java
+++ src/com/alexkaz/screenshoter/FileDistPanel.java
@@ -1,11 +1,13 @@
 package com.alexkaz.screenshoter;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.TitledBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FileDistPanel extends JPanel {
-
-    private JLabel title;
     private JLabel folderIcon;
     private JTextField textField;
     private JButton button;
@@ -20,29 +22,38 @@ public class FileDistPanel extends JPanel {
         setLocation(10,10);
         setSize(575,70);
         setLayout(null);
+        Border blackline = BorderFactory.createLineBorder(Color.black);
+        TitledBorder border = BorderFactory.createTitledBorder(blackline,"1. Choose the file destination:");
+        setBorder(border);
     }
 
     private void initComps() {
-        title = new JLabel("1. Choose the file destination:");
-        title.setBorder(BorderFactory.createLineBorder(Color.black));
-        title.setSize(550,20);
-        title.setLocation(10,10);
-        add(title);
-
         folderIcon = new JLabel("||");
         folderIcon.setBorder(BorderFactory.createLineBorder(Color.black));
         folderIcon.setSize(20,20);
-        folderIcon.setLocation(10,40);
+        folderIcon.setLocation(10,30);
         add(folderIcon);
 
-        textField = new JTextField("C://Users/");
+        textField = new JTextField("C:\\Users\\sasha\\Documents");
         textField.setSize(370,20);
-        textField.setLocation(40,40);
+        textField.setLocation(40,30);
         add(textField);
 
         button = new JButton("Change directory");
         button.setSize(140,20);
-        button.setLocation(420,40);
+        button.setLocation(420,30);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser dirChooser = new JFileChooser();
+                dirChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                dirChooser.setDialogTitle("Choose directory for screenshots saving");
+                int result = dirChooser.showDialog(null,"Choose");
+                if(result == JFileChooser.APPROVE_OPTION){
+                    textField.setText(dirChooser.getSelectedFile().getAbsolutePath());
+                }
+            }
+        });
         add(button);
     }
 }
